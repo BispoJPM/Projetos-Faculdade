8b739bb32c47ba1518272dc61da02775a8140021
João Pedro <142633184+BispoJPM@users.noreply.github.com>
2026-05-17 18:32:34 -0300

    Delete tdd-junit-exercicios directory

--- tdd-junit-exercicios/src/main/java/br/com/fatec/tdd/exercicio1/TipoTriangulo.java
+++
@@ -1,7 +0,0 @@
-package br.com.fatec.tdd.exercicio1;
-
-public enum TipoTriangulo {
-    ESCALENO,
-    ISOSCELES,
-    EQUILATERO
-}
--- tdd-junit-exercicios/src/main/java/br/com/fatec/tdd/exercicio3/Cargo.java
+++
@@ -1,8 +0,0 @@
-package br.com.fatec.tdd.exercicio3;
-
-public enum Cargo {
-    DESENVOLVEDOR,
-    DBA,
-    TESTADOR,
-    GERENTE
-}
